ddf82cfe84d2fdafc646e7ba6b675ba8e9a56aae
Shivam Tiwari <154796935+Synoep@users.noreply.github.com>
2025-06-29 23:55:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Split Array Largest Sum/split-array-largest-sum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+  int p( vector<int>&nums,int mid){
+        int sum=0;
+        int a=1;
+        for(int i=0;i<nums.size();i++){
+           if(sum+nums[i]<=mid){
+            sum+=nums[i];
+           }
+           else{
+              a++;
+              sum=nums[i];
+           }
+        }
+        return a;
+    }
+    int splitArray(vector<int>& nums, int k) {
+        // code here
+       int low=*max_element(nums.begin(),nums.end());
+      int high = accumulate(nums.begin(),nums.end(),0);
+      while (low<=high) {
+        int mid = (low + high) / 2;
+        int count=p(nums, mid);
+        if (count>k) low=mid+1;
+        else high=mid-1;
+    }
+    return low;
+    }
+};
